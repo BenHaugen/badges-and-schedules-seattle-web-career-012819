44cf841ccc80a603e11ccd522bd1c9272ceef909
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-23 19:07:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ end
 
 def assign_rooms(attendees)
   new_array = []
-  room = (1..7)
+  room = (0..6)
   attendees.each_with_index do |name, room|
   new_array.push("Hello, #{name}! You'll be assigned to room #{room}!")
 end 
