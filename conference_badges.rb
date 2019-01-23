8c6585193ecf0385bb2fff6b0fe6627eab2620c4
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-23 18:37:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ end
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(attendees)
   attendees.each do |name|
-    
+    return "Hello, my name is #{name}"
 end 
 
 
