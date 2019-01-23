f8a5079ddf2b313bca1bf84a6407a06155cdf969
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-23 18:25:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker(name)
 end 
 
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-def badge_badge_creator(attendees)
+def badge_batch_creator(attendees)
    badges = []
    badges.push(attendees)
    return badges
